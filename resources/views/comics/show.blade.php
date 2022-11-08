f98526db5ec992b09fd64bfcb77205e093829364
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-08 16:00:15 +0100

    Creazione struttura file show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,1 +1,32 @@
-prova
+@extends('layout.app')
+
+@section('title')
+    Dettagli fumetto
+@endsection
+
+@section('container')
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Thumb</th>
+                <th>Price</th>
+                <th>Series</th>
+                <th>Sale_date</th>
+                <th>Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->description }}</td>
+                <td><img src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></td>
+                <td>€{{ $comic->price }}</td>
+                <td>{{ $comic->series }}</td>
+                <td>{{ $comic->sale_date }}</td>
+                <td>{{ $comic->type }}</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
