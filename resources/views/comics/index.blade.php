5e3f3d6df44662fbb908829973d169f2952d8671
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-08 19:31:46 +0100

    Aggiunta pulsante per aggiungere un nuovo fumetto

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,7 +5,17 @@
 @endsection
 
 @section('container')
-    @foreach ($comics as $elementComic)
-        <a href="{{ route('comics.show', $elementComic->id) }}"><h3>{{$elementComic->title}}</h3></a>
-    @endforeach
+    <div>
+        <a href="">
+            <h2>Aggiungi nuovo fumetto</h2>
+        </a>
+    </div>
+
+    <div>
+        @foreach ($comics as $elementComic)
+            <a href="{{ route('comics.show', $elementComic->id) }}">
+                <h3>{{ $elementComic->title }}</h3>
+            </a>
+        @endforeach
+    </div>
 @endsection
