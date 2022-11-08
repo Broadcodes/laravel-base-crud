642bf274d6ced5b2f62d9e93f472e8b7b8535218
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-08 19:33:52 +0100

    Fix grafico sul form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,31 +8,31 @@
     <form action="{{ route('comics.store') }}" method="post">
         @csrf
         <div>
-            <label for="title"></label>
+            <label for="title">Title</label>
             <input type="text" name="title" id="title">
         </div>
         <div>
-            <label for="series"></label>
+            <label for="series">Series</label>
             <input type="text" name="series" id="series">
         </div>
         <div>
-            <label for="sale_date"></label>
+            <label for="sale_date">Sale date</label>
             <input type="text" name="sale_date" id="sale_date">
         </div>
         <div>
-            <label for="type"></label>
+            <label for="type">Type</label>
             <input type="text" name="type" id="type">
         </div>
         <div>
-            <label for="price"></label>
+            <label for="price">Price</label>
             <input type="text" name="price" id="price">
         </div>
         <div>
-            <label for="thumb"></label>
+            <label for="thumb">Thumb</label>
             <input type="text" name="thumb" id="thumb">
         </div>
         <div>
-            <label for="description"></label>
+            <label for="description">Description</label>
             <textarea name="description" id="description" cols="30" rows="10"></textarea>
         </div>
         <div>
