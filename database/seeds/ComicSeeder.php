cd656ede773cd8c395ea191728c415cb42fc927f
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-08 15:53:19 +0100

    Inserimento dati presi dal file config

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\model\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -11,6 +12,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = config('data.comics');
+
+        foreach ($data as $element) {
+            $comics = new Comic();
+            $comics->title = $element['title'];
+            $comics->description = $element['description'];
+            $comics->thumb = $element['thumb'];
+            $comics->price = $element['price'];
+            $comics->series = $element['series'];
+            $comics->sale_date = $element['sale_date'];
+            $comics->type = $element['type'];
+            $comics->save();
+        }
     }
 }
