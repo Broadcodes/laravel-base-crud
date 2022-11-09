1b8b243fcdd9b23a036cebfb700815e61d3f073d
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-09 19:51:58 +0100

    Inserimento css

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,22 +5,23 @@
 @endsection
 
 @section('container')
-    <div>
-        <a href="{{ route('comics.create') }}">
-            <h2>Aggiungi nuovo fumetto</h2>
-        </a>
+    <div class="add">
+        <h2><a href="{{ route('comics.create') }}">Clicca qui</a> se vuoi aggiungere un nuovo fumetto</h2>
     </div>
 
-    <div>
+    <div class="containerCards">
         @foreach ($comics as $elementComic)
-            <a href="{{ route('comics.show', $elementComic->id) }}">
-                <h3>{{ $elementComic->title }}</h3>
-            </a>
-            <form action="{{ route('comics.destroy', $elementComic->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <input onclick="return confirm('Vuoi davvero cancellare il fumetto?')" type="submit" value="Cancella">
-            </form>
+            <div class="card">
+                <a href="{{ route('comics.show', $elementComic->id) }}">
+                    <img src="{{ $elementComic->thumb }}" alt="{{ $elementComic->title }}">
+                    <h3>{{ $elementComic->title }}</h3>
+                </a>
+                <form action="{{ route('comics.destroy', $elementComic->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input onclick="return confirm('Vuoi davvero cancellare il fumetto?')" type="submit" value="Cancella">
+                </form>
+            </div>
         @endforeach
     </div>
 @endsection
