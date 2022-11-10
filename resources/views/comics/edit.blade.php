ccb3991271773a92082c6b0d49cd2757464d0691
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-10 16:17:51 +0100

    Aggiunta di validatori con testo custom in Edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,35 +11,65 @@
 
         <div>
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title', $comic->title) }}" required>
+            <input class="@error('title') is-validator @enderror" type="text" name="title" id="title"
+                value="{{ old('title', $comic->title) }}">
+            @error('title')
+                <h5>{{ __('Il titolo è obbligatorio') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="series">Series</label>
-            <input type="text" name="series" id="series" value="{{ old('series', $comic->series) }}">
+            <input class="@error('series') is-validator @enderror" type="text" name="series" id="series"
+                value="{{ old('series', $comic->series) }}">
+            @error('series')
+            <h5>{{ __('Il campo Serie deve essere una stringa e avere una lunghezza massima di 50 caratteri') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="sale_date">Sale date</label>
-            <input type="text" name="sale_date" id="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+            <input class="@error('sale_date') is-validator @enderror" type="text" name="sale_date" id="sale_date"
+                value="{{ old('sale_date', $comic->sale_date) }}">
+            @error('sale_date')
+            <h5>{{ __('Il campo Data deve avere un formato YYYY-MM-DD') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="type">Type</label>
-            <input type="text" name="type" id="type" value="{{ old('type', $comic->type) }}">
+            <input class="@error('type') is-validator @enderror" type="text" name="type" id="type"
+                value="{{ old('type', $comic->type) }}">
+            @error('type')
+            <h5>{{ __('Il campo Tipo deve essere una stringa e avere una lunghezza massima di 50 caratteri') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="price">Price</label>
-            <input type="text" name="price" id="price" value="{{ old('price', $comic->price) }}">
+            <input class="@error('price') is-validator @enderror" type="text" name="price" id="price"
+                value="{{ old('price', $comic->price) }}">
+            @error('price')
+            <h5>{{ __('Il campo Prezzo deve contenere un valore di tipo numerico') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="thumb">Thumb</label>
-            <input type="text" name="thumb" id="thumb" value="{{ old('thumb', $comic->thumb) }}">
+            <input class="@error('thumb') is-validator @enderror" type="text" name="thumb" id="thumb"
+                value="{{ old('thumb', $comic->thumb) }}">
+            @error('thumb')
+            <h5>{{ __('Il campo URL deve contenere un link valido') }}</h5>
+            @enderror
         </div>
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10"> {{ old('description', $comic->description) }} </textarea>
+            <textarea class="@error('description') is-validator @enderror" name="description" id="description" cols="30"
+                rows="10"> {{ old('description', $comic->description) }} </textarea>
+            @error('description')
+            <h5>{{ __('Il campo Descrizione deve essere una stringa di caratteri') }}</h5>
+            @enderror
         </div>
         <div>
             <input type="submit" value="Modifica">
         </div>
 
     </form>
+
+    <a href="{{ route('comics.show', $comic->id) }}">Torna indietro</a>
 @endsection
